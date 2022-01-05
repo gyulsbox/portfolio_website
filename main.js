8e0d60f7ae15e8d5801d323af18bee47ba5157c9
anwjr7878@gmail.com <anwjr7878@gmail.com>
2022-01-05 11:32:53 +0900

    Work Section Revised and Skill Section needs revise.

--- main.js
+++ main.js
@@ -60,33 +60,33 @@ arrowUp.addEventListener("click", () => {
 });
 
 // Projects
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
-workBtnContainer.addEventListener("click", (e) => {
-  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
+// const workBtnContainer = document.querySelector(".work__categories");
+// const projectContainer = document.querySelector(".work__projects");
+// const projects = document.querySelectorAll(".project");
+// workBtnContainer.addEventListener("click", (e) => {
+//   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+//   if (filter == null) {
+//     return;
+//   }
 
-  // Remove selection from the previous item and select the new one
-  const active = document.querySelector(".category__btn.selected");
-  active.classList.remove("selected");
-  const target = e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
-  target.classList.add("selected");
+//   // Remove selection from the previous item and select the new one
+//   const active = document.querySelector(".category__btn.selected");
+//   active.classList.remove("selected");
+//   const target = e.target.nodeName === "BUTTON" ? e.target : e.target.parentNode;
+//   target.classList.add("selected");
 
-  projectContainer.classList.add("anim-out");
-  setTimeout(() => {
-    projects.forEach((project) => {
-      if (filter === "*" || filter === project.dataset.type) {
-        project.classList.remove("invisible");
-      } else {
-        project.classList.add("invisible");
-      }
-    });
-    projectContainer.classList.remove("anim-out");
-  }, 300);
-});
+//   projectContainer.classList.add("anim-out");
+//   setTimeout(() => {
+//     projects.forEach((project) => {
+//       if (filter === "*" || filter === project.dataset.type) {
+//         project.classList.remove("invisible");
+//       } else {
+//         project.classList.add("invisible");
+//       }
+//     });
+//     projectContainer.classList.remove("anim-out");
+//   }, 300);
+// });
 
 const sectionIds = ["#home", "#about", "#skills", "#work", "#lastlys", "#contact"];
 
