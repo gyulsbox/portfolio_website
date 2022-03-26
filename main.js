1b1b1536937d289cd4bf84051e5d7bf60c937ef3
anwjr7878@gmail.com <anwjr7878@gmail.com>
2022-03-26 16:03:56 +0900

    20220325 Portfolio Website_index.html edited

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ navbarMenu.addEventListener("click", (event) => {
   selectNavItem(target);
 });
 
+
 // Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
 navbarToggleBtn.addEventListener("click", () => {
